7cc7bc5b09e9a542c406c3fecb825bc18678bc44
Damir Arnautovic <darnauto@fesb.hr>
2014-10-04 22:22:23 +0200

    added couple of examples in input file in.xml

--- run.php
+++ run.php
@@ -5,13 +5,52 @@ error_reporting(E_ALL);
 require_once('world.php');
 require_once('XMLDocumentHelper.php');
 
+// Initial data import from XML and initial World created
 $initial_data = SimpleXmlWorldParser::readXMLDocument('in.xml');
 $world = new World($initial_data->numberOfCells, $initial_data->organisms);
 
+
+for($iteration =0; $iteration < $initial_data->numberOfIterations; $iteration++ ) {
+    $output = "Iteration: {$world->getTickCount()}";
+    $output .= "\n".render($world, $initial_data);
+    $world->tick();
+    system('clear');
+    echo $output;
+    sleep(1);
+}
+
+$newWorldData = worldToWorldData($world, $initial_data);
+
+$outputXmlDocument = SimpleXmlWorldParser::createXMLDocument($newWorldData);
+$outputXmlDocument->saveXML();
+
+
+function worldToWorldData($world, $initial_data)
+{
+    $cellsArray = array();
+
+
+    foreach($world->getState() as $currentCellRow)
+    {
+        foreach($currentCellRow as $currentCell)
+        {
+           if(!empty($currentCell->getType())){
+               $cellsArray[] = new Organism($currentCell->getX(), $currentCell->getY(), $currentCell->getType());
+           }
+        }
+    }
+
+    $worldData = new WorldData($initial_data->numberOfIterations, $initial_data->numberOfSpecies, $initial_data->numberOfCells, $cellsArray);
+
+    return $worldData;
+}
+
+// Simple console world render function...
 function render($world, $initial_data) {
     $rendering = '';
     for ($y = 0; $y <= $initial_data->numberOfCells -1; $y++) {
         $rendering .= $y;
+        if($y <= 9){ $rendering .= " ";}
         for ($x = 0; $x <= $initial_data->numberOfCells -1; $x++) {
             $cells = $world->getState();
             $rendering .= '|' . (isset($cells[$x][$y]) ? $cells[$x][$y]->to_char() : '|');
@@ -19,19 +58,4 @@ function render($world, $initial_data) {
         $rendering .= "\n";
     }
     return $rendering;
-}
-
-
-
-$initial_data2 = SimpleXmlWorldParser::createXMLDocument($initial_data);
-
-
-for($iteration =0; $iteration < $initial_data->numberOfIterations; $iteration++ ) {
-    $output = "{$world->getTickCount()}";
-    $output .= "\n".render($world, $initial_data);
-    echo $output;
-    $world->tick();
-    sleep(1);
-//    system('clear');
-
 }
